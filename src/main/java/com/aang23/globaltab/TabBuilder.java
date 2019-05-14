e6f593afff29557127bd9b239a393495539b5c8c
Aang23 <qwerty15@gmx.fr>
2019-05-14 12:24:34 +0200

    Fixed a null exception

--- src/main/java/com/aang23/globaltab/TabBuilder.java
+++ src/main/java/com/aang23/globaltab/TabBuilder.java
@@ -1,6 +1,7 @@
 package com.aang23.globaltab;
 
 import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
 
 import net.kyori.text.TextComponent;
 import net.kyori.text.serializer.legacy.LegacyComponentSerializer;
@@ -24,8 +25,8 @@ public class TabBuilder {
         raw = raw.replace("%server%", getCurrentServer(player));
         raw = raw.replace("%ping%", String.valueOf(player.getPing()));
         raw = raw.replace("%playercount%", String.valueOf(GlobalTab.server.getPlayerCount()));
-        raw = raw.replace("%localplayercount%", String.valueOf(player.getCurrentServer().get().getServer().getPlayersConnected().size()));
-        raw = raw.replace("%totalmaxplayer%", String.valueOf(GlobalTab.server.getConfiguration().getShowMaxPlayers())); //Not accurate ! To change soon.
+        raw = raw.replace("%localplayercount%", getServerPlayerCount(player));
+        raw = raw.replace("%totalmaxplayer%", String.valueOf(GlobalTab.server.getConfiguration().getShowMaxPlayers()));
         raw = raw.replace("%motd%", GlobalTab.server.getConfiguration().getMotdComponent().toString());
         raw = raw.replace("%uuid%", player.getUniqueId().toString());
         raw = raw.replace("%ip%", player.getRemoteAddress().toString());
@@ -44,6 +45,16 @@ public class TabBuilder {
     private static String getBalance(Player player) {
         if (GlobalTab.playerBalances.containsKey(player.getUsername()))
             return String.valueOf(GlobalTab.playerBalances.get(player.getUsername()));
+        else
+            return "null";
+    }
+
+    private static String getServerPlayerCount(Player player) {
+        RegisteredServer server = null;
+        server = player.getCurrentServer().get().getServer();
+
+        if (server != null)
+            return String.valueOf(server.getPlayersConnected().size());
         else
             return "null";
     }
