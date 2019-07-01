18b12004d947df6427a9c51322b95e26ad7b288c
Draycia <lonelyyordle@gmail.com>
2019-07-01 06:48:45 -0700

    Refactor UserInfoGetter

--- src/main/java/com/aang23/globaltab/UserInfoGetter.java
+++ src/main/java/com/aang23/globaltab/UserInfoGetter.java
@@ -1,14 +1,18 @@
 package com.aang23.globaltab;
 
 import me.lucko.luckperms.api.Contexts;
+import me.lucko.luckperms.api.User;
+import me.lucko.luckperms.api.caching.MetaData;
+
+import java.util.Optional;
 
 public class UserInfoGetter {
     public static String getPrefixFromUsername(String username) {
-        if (GlobalTab.server.getPluginManager().isLoaded("luckperms")) {
-            if (GlobalTab.luckpermsapi.getUserSafe(username).isPresent()) {
-                Contexts contexts = GlobalTab.luckpermsapi.getContextForUser(GlobalTab.luckpermsapi.getUser(username)).get();
-                if (GlobalTab.luckpermsapi.getUser(username).getCachedData().getMetaData(contexts).getPrefix() != null)
-                    return GlobalTab.luckpermsapi.getUser(username).getCachedData().getMetaData(contexts).getPrefix();
+        Optional<MetaData> metaDataOptional = getUserMetaData(username);
+
+        if (metaDataOptional.isPresent()) {
+            if (metaDataOptional.get().getPrefix() != null) {
+                return metaDataOptional.get().getPrefix();
             }
         }
 
@@ -16,14 +20,30 @@ public class UserInfoGetter {
     }
 
     public static String getSuffixFromUsername(String username) {
-        if (GlobalTab.server.getPluginManager().isLoaded("luckperms")) {
-            if (GlobalTab.luckpermsapi.getUserSafe(username).isPresent()) {
-                Contexts contexts = GlobalTab.luckpermsapi.getContextForUser(GlobalTab.luckpermsapi.getUser(username)).get();
-                if (GlobalTab.luckpermsapi.getUser(username).getCachedData().getMetaData(contexts).getSuffix() != null)
-                    return GlobalTab.luckpermsapi.getUser(username).getCachedData().getMetaData(contexts).getSuffix();
+        Optional<MetaData> metaDataOptional = getUserMetaData(username);
+
+        if (metaDataOptional.isPresent()) {
+            if (metaDataOptional.get().getSuffix() != null) {
+                return metaDataOptional.get().getSuffix();
             }
         }
 
         return "";
+    }
+
+    private static Optional<MetaData> getUserMetaData(String username) {
+        if (!GlobalTab.server.getPluginManager().isLoaded("luckperms")) {
+            return Optional.empty();
+        }
+
+        Optional<User> userOptional = GlobalTab.luckpermsapi.getUserSafe(username);
+
+        if (!userOptional.isPresent()) {
+            return Optional.empty();
+        }
+
+        Optional<Contexts> contexts = GlobalTab.luckpermsapi.getContextForUser(userOptional.get());
+
+        return contexts.map(value -> userOptional.get().getCachedData().getMetaData(value));
     }
 }
